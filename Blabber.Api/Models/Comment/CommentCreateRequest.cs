979f8dd1d60384a9ebc7aa6b3b099b85c3f555a5
Blair Fleming <blair3003.bf@gmail.com>
2024-07-07 13:35:24 +0100

    Requests

--- Blabber.Api/Models/Comment/CommentCreateRequest.cs
+++ Blabber.Api/Models/Comment/CommentCreateRequest.cs
@@ -1,6 +1,24 @@
-﻿namespace Blabber.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blabber.Api.Models
 {
     public class CommentCreateRequest
     {
+        [Display(Name = "Blab ID")]
+        [Required(ErrorMessage = "The Blab ID field is required.")]
+        public int BlabId { get; set; }
+
+        [Display(Name = "Author ID")]
+        [Required(ErrorMessage = "The Author ID field is required.")]
+        public int AuthorId { get; set; }
+
+        [Display(Name = "Parent ID")]
+        [Required(ErrorMessage = "The Parent ID field is required.")]
+        public int ParentId { get; set; }
+
+        [Display(Name = "Body")]
+        [Required(ErrorMessage = "The Body field is required.")]
+        [StringLength(255, ErrorMessage = "The {0} must be at max {1} characters long.")]
+        public string? Body { get; set; }
     }
 }
--- Blabber.Api/Models/Comment/CommentEditRequest.cs
+++ Blabber.Api/Models/Comment/CommentEditRequest.cs
@@ -1,6 +1,12 @@
-﻿namespace Blabber.Api.Models.Comment
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blabber.Api.Models
 {
     public class CommentEditRequest
     {
+        [Display(Name = "Body")]
+        [Required(ErrorMessage = "The Body field is required.")]
+        [StringLength(255, ErrorMessage = "The {0} must be at max {1} characters long.")]
+        public string? Body { get; set; }
     }
 }
